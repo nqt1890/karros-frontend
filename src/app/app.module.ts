ff78b4195f97d6ed6449718a2c93c1ed5d0322fc
Thai Nguyen <nqt1890@gmail.com>
2021-02-11 23:16:01 +0700

    add banner carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/carousel/carousel.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from "@angular/core";
+import { trigger, transition, style, animate } from "@angular/animations";
+
+@Component({
+  selector: 'carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.css'],
+  animations: [
+    trigger('carouselAnimation', [
+      transition('void => *', [
+        style({ opacity: 0 }),
+        animate('300ms', style({ opacity: 1 }))
+      ]),
+      transition('* => void', [
+        animate('300ms', style({ opacity: 0 }))
+      ])
+    ])
+  ]
+})
+
+export class CarouselComponent implements OnInit {
+
+  constructor() { }
+
+  slides = [
+    { src: "assets/Bitmap.png" },
+    { src: "assets/banner2.jpg" },
+    { src: "assets/banner3.jpg" }
+  ];
+
+  currentSlide = 0;
+
+  ngOnInit(): void {
+    this.Repeat();
+  }
+
+  Repeat() {
+    setTimeout(() => {
+      this.__FunctionSlide();
+      this.Repeat();
+    }, 3500);
+  }
+
+  __FunctionSlide() {
+    if (this.currentSlide < 2) {
+      this.currentSlide += 1;
+    }
+    else {
+      this.currentSlide = 0;
+    }
+  }
+
+}
